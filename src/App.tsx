8ecb1ad4f35e1b239eba9df883282ddeeb1662b7
Milena Strassburger <milenastrass@gmail.com>
2024-03-19 20:07:19 -0300

    config-initial-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Login } from "./components/Login";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-red-600">Hello World</h1>
+      <Login />
     </>
-  )
+  );
 }
-
-export default App
