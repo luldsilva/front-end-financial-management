853bd756e019e482acc1a72dbe32d9313aa5a757
Lucas Silva <lucasstrlisilva@outlook.com>
2024-05-06 03:40:51 -0300

    custom for atp comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./components/Dashboard";
